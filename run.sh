eb391e069dab143c52dabdd7848318f08b8a6600
Junya Hayashi <junya.hayashi@groove-x.com>
2017-11-14 15:48:59 +0900

    change date format

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ set -e
 
 apt --yes install devscripts fakeroot python-bloom
 
-NOW=`date +'%Y%m%d-%H%M%S%z'`
+NOW=`date -u +'%Y%m%dT%H%M%SZ'`
 bloom-generate rosdebian \
                --os-name ${WERCKER_BLOOM_GENERATE_OS_NAME} \
                --os-version ${WERCKER_BLOOM_GENERATE_OS_VERSION} \
                --ros-distro ${WERCKER_BLOOM_GENERATE_ROS_DISTRO}
-sed -i -e "1 s/)/-${NOW})/" debian/changelog
+sed -i -e "1 s/-\w*)/-${NOW})/" debian/changelog
 fakeroot debian/rules binary
