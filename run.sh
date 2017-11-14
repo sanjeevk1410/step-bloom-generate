10e2b02c60fce2393742a9b16129bde79871ca15
Junya Hayashi <junya.hayashi@groove-x.com>
2017-11-14 14:32:28 +0900

    use quilt format

--- run.sh
+++ run.sh
@@ -4,11 +4,10 @@ set -e
 
 apt --yes install devscripts fakeroot python-bloom
 
-NOW=`date +'%Y%m%d-%H%M%S'`
+NOW=`date +'%Y%m%d-%H%M%S%z'`
 bloom-generate rosdebian \
                --os-name ${WERCKER_BLOOM_GENERATE_OS_NAME} \
                --os-version ${WERCKER_BLOOM_GENERATE_OS_VERSION} \
-               --ros-distro ${WERCKER_BLOOM_GENERATE_ROS_DISTRO} \
-               --native
+               --ros-distro ${WERCKER_BLOOM_GENERATE_ROS_DISTRO}
 sed -i -e "1 s/)/-${NOW})/" debian/changelog
 fakeroot debian/rules binary
